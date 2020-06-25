679ca1332fbe336857baa21b3617e8fc1ef216cb
Tomislav Maric <tomislav.maric@gmx.com>
2020-06-25 18:15:56 +0200

    3Dinit blockMesh parameters

--- cases/3Dinit/run_parameter_study.py
+++ cases/3Dinit/run_parameter_study.py
@@ -12,11 +12,13 @@ parser.add_argument('--dir_pattern', dest="dir_pattern", type=str, required=True
 parser.add_argument('--surface_file', dest="surface_file", type=str, required=True, 
                     help='Surface mesh file used for the volume fraction initialization.')
 
-parser.add_argument('--serial', dest="serial", type=bool, default=True,
-                    help='Generate the mesh.')
-
-parser.add_argument('--slurm', dest="slurm", type=bool, default=False,
+parser.add_argument('--serial', dest="serial_run", action='store_true',
                     help='Use SLURM workload manager to submit mesh generation jobs.')
+
+parser.add_argument('--slurm', dest="serial_run", action='store_false',
+                    help='Use SLURM workload manager to submit mesh generation jobs.')
+
+parser.set_defaults(serial_run=True)
 
 args = parser.parse_args()
 
@@ -37,8 +39,15 @@ if __name__ == '__main__':
     for parameter_dir in parameter_dirs: 
         pwd = os.getcwd()
         os.chdir(parameter_dir)
-        if (args.serial):
+        if (args.serial_run):
             call(vof_init_call)
-        elif (args.slurm):
-            call(["sbatch", os.path.join(os.pardir, "surfaceCellVofInit.sbatch")])
+        else: 
+            # Memory and run time can now be made dependent on mesh size. 
+            base_command="srun --mem-per-cpu=1000 --time=05:00 --ntasks=1"
+
+            variable_command=" --job-name surfaceCellVofInit " + \
+                " -o %s.log " + \
+                " surfaceCellVofInit -checkVolume -surfaceFile " + \
+                " %s >/dev/null 2>&1 &" % (args.surface_file, args.surface_file)
+            call(base_command + variable_command, shell=True)
         os.chdir(pwd)
