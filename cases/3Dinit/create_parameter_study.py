b64acb665d06a0628930fa9c72caa4ab3ef9447a
Tomislav Maric <tomislav.maric@gmx.com>
2020-09-25 16:36:28 +0200

    Running SLURM study generation

--- cases/3Dinit/create_parameter_study.py
+++ cases/3Dinit/create_parameter_study.py
@@ -19,10 +19,7 @@ parser.add_argument('--surface', dest="surface", type=str, required=True,
 parser.add_argument('--mesh_generator', dest="mesh_generator", type=str, default="", required=True,
                     help='Supported mesh generators: blockMesh, cartesianMesh, tetMesh, polyMesh')
 
-parser.add_argument('--serial', dest="serial_run", action='store_true',
-                    help='Use SLURM workload manager to submit mesh generation jobs.')
-
-parser.add_argument('--slurm', dest="serial_run", action='store_false',
+parser.add_argument('--slurm', dest="slurm_run", action='store_true',
                     help='Use SLURM workload manager to submit mesh generation jobs.')
 
 parser.set_defaults(serial_run=True)
@@ -30,6 +27,7 @@ parser.set_defaults(serial_run=True)
 args = parser.parse_args()
 
 if __name__ == '__main__':
+
     args = parser.parse_args(sys.argv[1:])
 
     prefix = args.surface + "_" + args.parameter_file
@@ -59,12 +57,10 @@ if __name__ == '__main__':
         os.chdir(parameter_dir)
         geo_file = "%s.geo" % args.surface
 
-        if (args.serial_run):
-            call([args.mesh_generator])
-            print("Using GMSH file %s for surface generation." % geo_file)
-            if (os.path.exists(geo_file) and os.path.isfile(geo_file)):
-                call(["gmsh", "-2", geo_file, "-o", "%s.vtk" % args.surface])
-        else: 
+        if (args.slurm_run): 
+
+            print("SLURM mesh generation...")
+
             base_command="srun --mem-per-cpu=500 --time=00:10 --ntasks=1"
 
             # Submit volume mesh generation to the SLURM workload manager.
@@ -76,5 +72,14 @@ if __name__ == '__main__':
             variable_command=" --job-name %s -o %s.log gmsh -2 %s.geo -o %s.vtk >/dev/null 2>&1 &" % \
                     (args.surface, args.surface, args.surface, args.surface)
             call(base_command + variable_command, shell=True)
+
+        else:
+
+            print("Serial mesh generation...")
+
+            call([args.mesh_generator])
+            print("Using GMSH file %s for surface generation." % geo_file)
+            if (os.path.exists(geo_file) and os.path.isfile(geo_file)):
+                call(["gmsh", "-2", geo_file, "-o", "%s.vtk" % args.surface])
 
         os.chdir(pwd)
