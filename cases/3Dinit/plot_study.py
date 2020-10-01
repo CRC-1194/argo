000ca56e5e3876e26952d50dc2f249da19e702b6
Tomislav Maric <tomislav.maric@gmx.com>
2020-10-01 19:05:05 +0200

    Sort Nc

--- cases/3Dinit/plot_study.py
+++ cases/3Dinit/plot_study.py
@@ -16,17 +16,19 @@ def plot_study(surface, mesher, exact_volume=1, data_dir="",
     
     param_dirs = [param_dir for param_dir in os.listdir(os.curdir) if os.path.isdir(param_dir) \
                    and surface in param_dir and mesher in param_dir] 
+
     param_dirs.sort()
 
     csv_files = [os.path.join(param_dir,csv_file) for param_dir in param_dirs \
                  if os.path.exists(os.path.join(param_dir,csv_file))]
-    
+
     if (len(csv_files) == 0):
         raise ValueError("No CSV files found, did the parameter variation run?")
 
     data = pd.concat((pd.read_csv(csv_file) for csv_file in csv_files))
 
     n_cells = list(set(data["N_CELLS"]))
+    n_cells.sort()
     n_triangles = list(set(data["N_TRIANGLES"]))
     
     data["N_TRIANGLES_ROOT"] = data["N_TRIANGLES"] ** 0.5
