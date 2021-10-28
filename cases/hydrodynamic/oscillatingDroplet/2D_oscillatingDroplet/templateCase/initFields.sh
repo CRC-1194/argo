a4242a9bbbbc16c973ccd36b7d9ac2d141955040
Tobias Tolle <bt@lefou-familie.org>
2021-10-28 17:18:37 +0200

    [Pandora]
    Added additional test cases for testing surface tension.

---
+++ cases/hydrodynamic/oscillatingDroplet/2D_oscillatingDroplet/templateCase/initFields.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#runApplication blockMesh
+#runApplication -s 1 topoSet -dict system/topoSetDict1
+#runApplication -s 1 refineMesh -dict system/refineMeshDict1 -overwrite
+#runApplication -s 2 topoSet -dict system/topoSetDict2
+#runApplication -s 2 refineMesh -dict system/refineMeshDict2 -overwrite
+
+setAlphaField
+
+#------------------------------------------------------------------------------
