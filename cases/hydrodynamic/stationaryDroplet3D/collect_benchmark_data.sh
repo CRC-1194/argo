604b6beafdf0c6fe14af24b38d64388f4dbc1605
Tobias Tolle <bt@lefou-familie.org>
2022-02-24 19:31:49 +0100

    Added data collection scripts to stationary droplet cases, updated run scripts.

---
+++ cases/hydrodynamic/stationaryDroplet3D/collect_benchmark_data.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Remove everything except the header from the data files.
+# Remove the first whitespace so the header is read correctly by pandas
+for DIR in benchmark-inter*/;
+do
+    cd $DIR/postProcessing/minMaxU/0
+    tail -n +2 fieldMinMax.dat > fieldMinMax_fixed_header.dat
+    sed -i 's/ //' fieldMinMax_fixed_header.dat
+    cd ../../../../
+done
+
+# Need to collect data for interFoam and interIsoFoam separately due to different case base names.
+argo-agglomerate-study-data.py benchmark-interFoam-benchmarkpaper_00000_templateCase/postProcessing/minMaxU/0/fieldMinMax_fixed_header.dat -p benchmarkpaper.parameter -f stationary_droplet_3D_interFoam
+argo-agglomerate-study-data.py benchmark-interIsoFoam-benchmarkpaper_00009_templateCase/postProcessing/minMaxU/0/fieldMinMax_fixed_header.dat -p benchmarkpaper.parameter -f stationary_droplet_3D_interIsoFoam
