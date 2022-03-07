8b502a7ef2ddc4089a30e828934304fed071b334
Tobias Tolle <bt@lefou-familie.org>
2022-03-07 16:23:47 +0100

    Updated translatingDroplet3D for benchmark.

--- cases/hydrodynamic/translatingDroplet3D/templateCase/derivedParameters.py
+++ cases/hydrodynamic/translatingDroplet3D/templateCase/derivedParameters.py
@@ -1,45 +1,58 @@
-# Documentation on how to use derivedParameters.py can be found on the SFB
-# confluence
+# Documentation on how to use derivedParameters.py can be found in the
+# NFDI4Ing knowledge base:
+# https://nfdi4ing.pages.rwth-aachen.de/knowledge-base/how-tos/how_to_set_dependent_parameters_in_a_parameter_study_using_pyfoam_with_derivedparameters_py/
+
 def computeDeltaT(values):
     import math
+    import sys
 
     # Give explicitly prescribed time step sizes precedence over
     # computed values
-    if "delta_t" in values:
-        return values["delta_t"]
+    if "DELTA_T" in values:
+        return values["DELTA_T"]
 
-    domainLength = 5.0*values["radius"]
-    resolution = values["resolution"]
-    rho_droplet = values["rho_droplet"]
+    
+    domainLength = values["DOMAIN_LENGTH"]
+    resolution = values["RESOLUTION"]
+    h = domainLength/resolution
 
     # rho_ambient might be expressed in terms of rho_droplet
     rho_ambient = 0.0
-    if values["rho_ambient"] == "$rho_droplet":
-        rho_ambient = rho_droplet
+    rho_droplet = 0.0
+    sigma = 0.0
+    if values["FLUID_PAIRING"] == "water-air":
+        rho_ambient = 1.0
+        rho_droplet = 1000.0
+        sigma = 72.75e-3
+    elif values["FLUID_PAIRING"] == "oil_novec7500-water": 
+        rho_ambient = 1000.0
+        rho_droplet = 1614.0
+        sigma = 49.5e-3
+    elif values["FLUID_PAIRING"] == "gearoil-air":
+        rho_ambient = 1.0
+        rho_droplet = 888.0
+        sigma = 30.0e-3
     else:
-        rho_ambient = values["rho_ambient"]
-    sigma = values["surface_tension_coefficient"]
+        sys.exit("Error: unknown FLUID PAIRING:" + values["FLUID_PAIRING"])
 
-    # Use the resolution of the Eulerian mesh rather than the front element size.
-    # So far there is no indication that the triangle size plays a role for the
-    # maximum time step.
+    # Coefficient to scale the capillary time step size
+    sf = values["SCALE_CAPILLARY_DELTA_T"]
+
+    # For translating droplet: compute deltat for the prescribed translation velocity
+    cfl = values["TARGET_CFL"]
+    vref = values["TRANSLATION_VELOCITY"]
+    cfl_delta_t = cfl*h/vref
+
     # The computation follows eq. (18) in Popinet 2009 and
     # eq. in (43) Denner & van Wachem 2015, respectively.
-    h = domainLength/(resolution)
-
-    # Safety coefficient to stay below critical delta_t threshold
-    sf = values["scale_delta_t"]
-    capillary_delta_t = sf*math.sqrt((rho_droplet + rho_ambient)*math.pow(h,3.0)/(2.0*math.pi*sigma))
-
-    # Check for the CFL number
-    cfl_factor = values["cfl_factor"]
-    u_characteristic = values["z_velocity"]
-    cfl_delta_t = cfl_factor*h/u_characteristic
+    capillary_delta_t = 1e15
+    if sigma > 0.0:
+        capillary_delta_t = sf*math.sqrt((rho_droplet + rho_ambient)*math.pow(h,3.0)/(2.0*math.pi*sigma))
 
     return min(capillary_delta_t, cfl_delta_t)
 
-delta_t = computeDeltaT(locals())
+DELTA_T = computeDeltaT(locals())
 
-# This dummy is required to avoid a PyFoam error if delta_t is given explicitly in the
+# This dummy is required to avoid a PyFoam error if DELTA_T is given explicitly in the
 # parameter file
 dummy = 0
