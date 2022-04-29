0c7747189b6f4bc6771907eb5213113a19d83170
Tobias Tolle <bt@lefou-familie.org>
2022-04-29 12:22:09 +0200

    Updated convection test cases: added run and data collection scripts, fixed write location of secondary data.

---
+++ cases/convection/discInReversedVortexFlow/run_benchmarkpaper.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Create the study split by the solver to be used:
+argo-create-parameter-study.py benchmarkpaper.parameter -p benchmark-interFoam -v 0,1,2 
+argo-create-parameter-study.py benchmarkpaper.parameter -p benchmark-interIsoFoam -v 3,4,5
+
+# Initialize variants: create mesh and initialize fields
+argo-initialize-parameter-study.py benchmark- -m blockMesh -f initFields.sh -par
+
+# Wait a bit to ensure that initialization jobs have finished.
+# Only relevant for runs on a cluster
+#echo "Wait 30 seconds for init jobs to finish."
+#sleep 30
+#wait
+
+nohup argo-run-study.py interFoam -d benchmark-interFoam -np 4 > logs.interFoam &
+nohup argo-run-study.py interIsoFoam -d benchmark-interIsoFoam -np 4 > logs.interIsoFoam &
