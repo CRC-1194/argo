dbd18b4d803ccb2bd6a4a18944562bc11e467f2d
Tobias Tolle <bt@lefou-familie.org>
2022-05-23 16:52:14 +0200

    Adapted secondary data computation of oscillatingWave2D according to reference solution.

--- cases/hydrodynamic/oscillatingWave2D/collect_benchmark_data.sh
+++ cases/hydrodynamic/oscillatingWave2D/collect_benchmark_data.sh
@@ -5,11 +5,15 @@
 for DIR in benchmark-inter*/;
 do
     cd $DIR/postProcessing/interfaceHeight1/0
-    echo "#time    height_above_boundary       height_above_location" > height_with_header.dat
-    cat height.dat >> height_with_header.dat
+    # FIXME: ensure that the equilibrium interface height, 0.0013 currently, is set correctly.
+    python -c "import pandas as pd; \
+        df = pd.read_csv('height.dat', delim_whitespace=True, comment='#', header=None, \
+                          names=['time', 'height_above_boundary', 'height_above_location']); \
+        df['amplitude_at_center'] = df['height_above_location'] - 0.0013; \
+        df.to_csv('interface_data.csv', index=False)"
     cd ../../../../
 done
 
 # Need to collect data for interFoam and interIsoFoam separately due to different case base names.
-argo-agglomerate-study-data.py benchmark-interFoam-benchmarkpaper_00000_templateCase/postProcessing/interfaceHeight1/0/height_with_header.dat -p benchmarkpaper.parameter -f oscillating_wave_2D_interFoam
-argo-agglomerate-study-data.py benchmark-interIsoFoam-benchmarkpaper_00009_templateCase/postProcessing/interfaceHeight1/0/height_with_header.dat -p benchmarkpaper.parameter -f oscillating_wave_2D_interIsoFoam
+argo-agglomerate-study-data.py benchmark-interFoam-benchmarkpaper_00000_templateCase/postProcessing/interfaceHeight1/0/interface_data.csv -p benchmarkpaper.parameter -f oscillating_wave_2D_interFoam
+argo-agglomerate-study-data.py benchmark-interIsoFoam-benchmarkpaper_00009_templateCase/postProcessing/interfaceHeight1/0/interface_data.csv -p benchmarkpaper.parameter -f oscillating_wave_2D_interIsoFoam
