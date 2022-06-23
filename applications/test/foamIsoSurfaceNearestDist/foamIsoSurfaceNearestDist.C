a1084b57e2f5650004f0e25a7fff2ad9b5ae59a7
Tomislav Maric <tomislav.maric@gmx.com>
2022-06-23 12:05:11 +0200

    foamIsoSurfaceNearestDist
    Initial isoSurfaceTopo reconstruction and test case

---
+++ applications/test/foamIsoSurfaceNearestDist/foamIsoSurfaceNearestDist.C
@@ -0,0 +1,114 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2022 Tomislav Maric, TU Darmstadt
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    foamIsoSurfaceNearestDist 
+
+Description
+    Compute the nearest signed-distance from the Finite Volume mesh and and 
+    iso-surface (triangulated surface mesh). 
+
+Authors
+    Tomislav Maric, MMA, TU Darmstadt, maric@mma.tu-darmstadt.de
+
+\*---------------------------------------------------------------------------*/
+
+#include "IOobject.H"
+#include "error.H"
+#include "fvCFD.H"
+
+#include "isoSurfaceCell.H"
+#include "isoSurfaceTopo.H"
+#include "isoSurfacePoint.H"
+#include "volPointInterpolation.H"
+#include <cstdlib>
+#include <iomanip>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::addOption
+    (
+        "field",
+        "string",
+        "Name of the volume (cell-centered) field whose curvature is approximated."
+    );
+
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    const word fieldName = args.get<word>("field");
+
+    #include "createFields.H"
+
+    volPointInterpolation vpInterp(mesh); 
+
+    tmp<pointScalarField> psiPointTmp = vpInterp.interpolate(psi);
+    pointScalarField& psiPoint = psiPointTmp.ref();
+    psiPoint.rename(fieldName + ".point");
+    psiPoint.write();
+
+    isoSurfaceTopo isoTopo(
+        mesh, 
+        psi, 
+        psiPoint, 
+        0.5
+    );
+
+    std::stringstream ss; 
+    ss << "isoTopo-" << std::setw(8) << std::setfill('0') 
+        << Pstream::myProcNo() << ".vtk";
+    isoTopo.write(ss.str());
+    
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< nl;
+    runTime.printExecutionTime(Info);
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
+
+
+    //const auto& Sf = isoTopo.faceNormals(); 
+    //const auto& meshCells = isoTopo.meshCells();
+    //forAll(meshCells, polygonI)
+    //{
+        //nc[meshCells[polygonI]] = Sf[polygonI];
+    //}
+    
+    //volScalarField ncMag (Foam::mag(nc));
+    //forAll(nc, cellI)
+    //{
+        //if (ncMag[cellI] > SMALL)
+            //nc[cellI] /= ncMag[cellI];
+    //}
+    //surfaceVectorField nf (fvc::interpolate(nc));
