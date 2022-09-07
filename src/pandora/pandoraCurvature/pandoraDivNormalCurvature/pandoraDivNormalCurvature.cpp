31d6b3f3fc29cfb3e67269688be9e4664134beae
ChaiGuoliang <peter79634@stu.xjtu.edu.cn>
2022-09-07 17:11:28 +0200

    still parallel fix try

--- src/pandora/pandoraCurvature/pandoraDivNormalCurvature/pandoraDivNormalCurvature.cpp
+++ src/pandora/pandoraCurvature/pandoraDivNormalCurvature/pandoraDivNormalCurvature.cpp
@@ -180,7 +180,7 @@ scalar sphereRadius = 0.002; // Sphere radius
         markers.correctBoundaryConditions();
     }
 
-    zoneDistribute distribute = zoneDistribute::New(mesh());
+    zoneDistribute& distribute = zoneDistribute::New(mesh());
 
     boolList updateZone(mesh().nCells(), false);
     forAll (updateZone, uzi)
@@ -197,9 +197,9 @@ scalar sphereRadius = 0.002; // Sphere radius
             "SMALL", interfaceNormals.dimensions(), SMALL
         )
     );
-    //averagedNormals_.normalise();
     averagedNormals_.correctBoundaryConditions();
 
+/*
 // PLIC normals refinement.
 {
     averagedNormals_ = vector::zero;
@@ -254,14 +254,31 @@ scalar sphereRadius = 0.002; // Sphere radius
                 valuesZ.append(n.z());
             }
         }
-if (centres.size() == 3)
-    Pout<<"!!!THREE1!!!"<<nl;
 
-        averagedNormals_[cellI][0] = interpolator.LSinterpolate(p, centres, valuesX);
-        averagedNormals_[cellI][1] = interpolator.LSinterpolate(p, centres, valuesY);
-        averagedNormals_[cellI][2] = interpolator.LSinterpolate(p, centres, valuesZ);
+        centres.shrink();
+        valuesX.shrink();
+        valuesY.shrink();
+        valuesZ.shrink();
+
+        if (centres.capacity() < 4 && centres.capacity() > 0)
+        {
+            Pout<<"!!!THREE1!!!"<<nl;
+
+            averagedNormals_[cellI][0] = interpolator.IDWinterpolate(p, centres, valuesX, rr);
+            averagedNormals_[cellI][1] = interpolator.IDWinterpolate(p, centres, valuesY, rr);
+            averagedNormals_[cellI][2] = interpolator.IDWinterpolate(p, centres, valuesZ, rr);
+
+            continue;
+        }
+
+        averagedNormals_[cellI][0] = interpolator.IDeCinterpolate(p, centres, valuesX, rr);
+        averagedNormals_[cellI][1] = interpolator.IDeCinterpolate(p, centres, valuesY, rr);
+        averagedNormals_[cellI][2] = interpolator.IDeCinterpolate(p, centres, valuesZ, rr);
+
+        //averagedNormals_[cellI][0] = interpolator.LSinterpolate(p, centres, valuesX);
+        //averagedNormals_[cellI][1] = interpolator.LSinterpolate(p, centres, valuesY);
+        //averagedNormals_[cellI][2] = interpolator.LSinterpolate(p, centres, valuesZ);
     }
-    //averagedNormals_.normalise();  
     averagedNormals_ /=  
     (
         mag(averagedNormals_) + 
@@ -271,6 +288,7 @@ if (centres.size() == 3)
     );
     averagedNormals_.correctBoundaryConditions();
 }
+*/
 
     // Interface normals propagate. 
     for (label i = 0; i < nPropagate_; ++i)
@@ -293,10 +311,6 @@ if (centres.size() == 3)
         const labelListList& stencil = distribute.getStencil();
 
         volVectorField avgNormTmp = averagedNormals_;
-
-        bool isLess = false;
-        volScalarField lessThanFour = markers;
-        lessThanFour = 0.0;
 
         forAll (markers, cellI)
         {
@@ -339,21 +353,28 @@ if (centres.size() == 3)
             valuesY.shrink();
             valuesZ.shrink();
 
-            avgNormTmp[cellI][0] = interpolator.LSinterpolate(p, centres, valuesX);
-            avgNormTmp[cellI][1] = interpolator.LSinterpolate(p, centres, valuesY);
-            avgNormTmp[cellI][2] = interpolator.LSinterpolate(p, centres, valuesZ);
-
             if (centres.capacity() < 4 && centres.capacity() > 0)
             {
                 Pout<<"!!!THREE2!!!"<<nl;
-                lessThanFour[cellI] = 1;
-                isLess = true;
-                avgNormTmp[cellI] = vector::zero;
+
+                avgNormTmp[cellI][0] = interpolator.IDWinterpolate(p, centres, valuesX, rr);
+                avgNormTmp[cellI][1] = interpolator.IDWinterpolate(p, centres, valuesY, rr);
+                avgNormTmp[cellI][2] = interpolator.IDWinterpolate(p, centres, valuesZ, rr);
+                
+                continue;
             }
+
+            //avgNormTmp[cellI][0] = interpolator.IDeCinterpolate(p, centres, valuesX, rr);
+            //avgNormTmp[cellI][1] = interpolator.IDeCinterpolate(p, centres, valuesY, rr);
+            //avgNormTmp[cellI][2] = interpolator.IDeCinterpolate(p, centres, valuesZ, rr);
+
+            avgNormTmp[cellI][0] = interpolator.LSinterpolate(p, centres, valuesX);
+            avgNormTmp[cellI][1] = interpolator.LSinterpolate(p, centres, valuesY);
+            avgNormTmp[cellI][2] = interpolator.LSinterpolate(p, centres, valuesZ);
         }
+        avgNormTmp.correctBoundaryConditions();
 
         averagedNormals_ == avgNormTmp;
-        //averagedNormals_.normalise();
         averagedNormals_ /=  
         (
             mag(averagedNormals_) + 
@@ -362,94 +383,12 @@ if (centres.size() == 3)
             )
         );
         averagedNormals_.correctBoundaryConditions();
-        lessThanFour.correctBoundaryConditions();
-
-        if (isLess)
-        {
-            boolList zone(mesh().nCells(), false);
-            forAll (zone, zi)
-            {
-                if (lessThanFour[zi] == 1)
-                {
-                    zone[zi] = true;
-                }
-            }
-
-            distribute.setUpCommforZone(zone, false);
-            Map<vector> mapMC = 
-                distribute.getDatafromOtherProc(zone, mesh().C());
-            Map<vector> mapNormals = 
-                distribute.getDatafromOtherProc(zone, averagedNormals_);
-
-            const labelListList& stencil = distribute.getStencil();
-
-            volVectorField avgNormTmp = averagedNormals_;
-
-            forAll (lessThanFour, cellI)
-            {
-                if (lessThanFour[cellI] != 1) continue;
-
-                avgNormTmp[cellI] = vector::zero;
-
-                point p = mesh().C()[cellI];
-
-                DynamicField<vector> centres;
-                DynamicField<scalar> valuesX;
-                DynamicField<scalar> valuesY;
-                DynamicField<scalar> valuesZ;
-
-                for (const label gblIdx : stencil[cellI])
-                {
-                    vector n = distribute.getValue(averagedNormals_, mapNormals, gblIdx);
-
-                    if (mag(n) != 0)
-                    {
-                        n /= mag(n);
-
-                        vector centre = distribute.getValue(mesh().C(), mapMC, gblIdx);
-
-                        vector dist = centre - p;
-                        if (mag(dist) == 0) continue;
-
-                        vector distToSurf = dist & n / mag(n) * n;
-                        vector verticalDist = dist - distToSurf;
-
-                        vector cc = p - verticalDist;
-
-                        centres.append(cc);
-                        valuesX.append(n.x());
-                        valuesY.append(n.y());
-                        valuesZ.append(n.z());
-                    }
-                }
-
-                centres.shrink();
-                valuesX.shrink();
-                valuesY.shrink();
-                valuesZ.shrink();
-
-                avgNormTmp[cellI][0] = interpolator.LSinterpolate(p, centres, valuesX);
-                avgNormTmp[cellI][1] = interpolator.LSinterpolate(p, centres, valuesY);
-                avgNormTmp[cellI][2] = interpolator.LSinterpolate(p, centres, valuesZ);
-            }
-
-            averagedNormals_ == avgNormTmp;
-            //averagedNormals_.normalise();
-            averagedNormals_ /=  
-            (
-                mag(averagedNormals_) + 
-                dimensionedScalar(
-                    "SMALL", averagedNormals_.dimensions(), SMALL
-                )
-            );
-        }
     }
 
     for (label i = 0; i < nAverage_; i++)
     {
         averagedNormals_ = fvc::average(averagedNormals_);
 
-        //averagedNormals_.normalise();
         averagedNormals_ /=  
         (
             mag(averagedNormals_) + 
