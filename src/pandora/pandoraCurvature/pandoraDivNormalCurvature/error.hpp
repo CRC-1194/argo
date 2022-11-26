6f169e997a3110a2d46b13d008aaa5f63499f7ed
Guoliang Chai <guoliangchai@stu.xjtu.edu.cn>
2022-11-26 21:18:04 +0100

    rename

--- src/pandora/pandoraCurvature/pandoraDivNormalCurvature/error.hpp
+++ src/pandora/pandoraCurvature/pandoraDivNormalCurvature/error.hpp
@@ -3,13 +3,20 @@ scalar absError = 0;
 scalar l1 = 0;
 scalar l2 = 0;
 scalar lInf = 0;
+label count0 = 0;
+label count1 = 0;
+label count2 = 0;
+label count3 = 0;
+label count4 = 0;
+label count5 = 0;
+label count6 = 0;
 label count = 0;
 volVectorField exactNormals = averagedNormals_;
 volVectorField calcNormals = averagedNormals_;
 forAll(markers, i)
 {
     if (markers[i] == -1) continue;
-    if (markers[i] != 0) continue;
+    //if (markers[i] != 0) continue;
 
     //vector n1 = sphereCentre - interfaceCentres[i];
     vector n1 = sphereCentre - mesh().C()[i];
@@ -21,21 +28,49 @@ forAll(markers, i)
     n2 /= mag(n2);
     calcNormals[i] = n2;
 
+    count++;
+
+    if (markers[i] == 0)
+        count0++;
+    if (markers[i] == 1)
+        count1++;
+    if (markers[i] == 2)
+        count2++;
+    if (markers[i] == 3)
+        count3++;
+    if (markers[i] == 4)
+        count4++;
+    if (markers[i] == 5)
+        count5++;
+    if (markers[i] == 6)
+        count6++;
     absError = 1 - (n1 & n2);
     l1 += absError / mag(n1);
     l2 += sqr(absError) / magSqr(n1);
-    count++;
     if ((absError/mag(n1)) > lInf)
     {
         lInf = absError/mag(n1);
     }
 }
+reduce(count0, sumOp<label>());
+Info<<"count0 = "<<count0<<nl;
+reduce(count1, sumOp<label>());
+Info<<"count1 = "<<count1<<nl;
+reduce(count2, sumOp<label>());
+Info<<"count2 = "<<count2<<nl;
+reduce(count3, sumOp<label>());
+Info<<"count3 = "<<count3<<nl;
+reduce(count4, sumOp<label>());
+Info<<"count4 = "<<count4<<nl;
+reduce(count5, sumOp<label>());
+Info<<"count5 = "<<count5<<nl;
+reduce(count6, sumOp<label>());
+Info<<"count6 = "<<count6<<nl;
+reduce(count, sumOp<label>());
+Info<<"count = "<<count<<nl;
 reduce(l1, sumOp<scalar>());
 reduce(l2, sumOp<scalar>());
-reduce(count, sumOp<label>());
 reduce(lInf, maxOp<scalar>());
-
-Info<<"count = "<<count<<nl;
 if (count > 0)
 {
     Info<<"l1 = "<<l1/count*100<<nl;
@@ -111,6 +146,7 @@ if (countCurv > 0)
     Info<<"l2Curv = "<<sqrt(l2Curv/count)*100<<nl;
     Info<<"lInfCurv = "<<lInfCurv<<nl;
 }
+*/
 
 exactNormals.rename("normExct");
 if (cellCurvature_.time().writeTime())
@@ -118,8 +154,9 @@ if (cellCurvature_.time().writeTime())
 calcNormals.rename("normCalc");
 if (cellCurvature_.time().writeTime())
     calcNormals.write();
-*/
 
+/*
 markers.rename("cellMarker");
 if (mesh().time().writeTime())
     markers.write();
+*/
---
+++ src/pandora/pandoraCurvature/pandoraDivNormalCurvature/new_face/pandoraDivNormalCurvature.hpp
@@ -0,0 +1,124 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2020 Tomislav Maric, Tobias Tolle, TU Darmstadt
+                       Anja Lippert, BOSCH CR 
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Class
+    Foam::pandoraDivNormalCurvature
+
+Description
+    Extend the interface normal vector field by diffusion and calculate the 
+    curvature as div(normal). Re-sets the normals in interface cells during 
+    smoothing. 
+
+SourceFiles
+    pandoraDivNormalCurvature.C
+
+\*---------------------------------------------------------------------------*/
+
+#ifndef pandoraDivNormalCurvature_H
+#define pandoraDivNormalCurvature_H
+
+#include "pandoraCurvature.hpp"
+#include "zoneDistribute.H"
+#include "labelVector.H"
+
+#include "interpolationSchemes.hpp"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+/*---------------------------------------------------------------------------*\
+                         Class pandoraDivNormalCurvature Declaration
+\*---------------------------------------------------------------------------*/
+
+class pandoraDivNormalCurvature
+    :
+        public pandoraCurvature
+{
+
+protected:
+
+    const word fieldName_; 
+    const label nPropagate_;
+    const label nAverage_; 
+    volVectorField averagedNormals_; 
+
+
+        Switch curvFromTr_;
+
+        // Cell markers
+        volScalarField markers_;
+
+        // Copy of the previous interface cells 
+        boolList interCells_;
+
+        volScalarField cellDistLevel_;
+        boolList nextToInter_;
+        labelField cellPointCells_;
+        zoneDistribute& distribute_;
+        interpolationSchemes interp_;
+        labelList index_;
+        labelList maxCpc_;
+
+        void normalise(vectorField&);
+        void updateMarkersAndCounts();
+        void normalPropagate(const bool&, volVectorField&);
+        void curvInterpolate(const volVectorField&);
+        void curvAverage();
+        void curvExtend(const volVectorField&, const volVectorField&);
+
+public:
+
+    // Static Data Members
+
+    TypeName ("divNormal");
+
+    // Constructors
+
+    //- Construct from components
+    pandoraDivNormalCurvature(const fvMesh& mesh, const dictionary& dict);
+
+    //- Construct as copy
+    pandoraDivNormalCurvature(const pandoraDivNormalCurvature&) = default;
+
+    //- Destructor
+    virtual ~pandoraDivNormalCurvature() = default;
+
+    // Member Functions
+    
+    virtual volScalarField& cellCurvature(); 
+};
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#endif
+
+// ************************************************************************* //
